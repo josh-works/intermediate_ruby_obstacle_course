106d79aec8acf70e887dd9642b1d55585a650405
Josh Thompson <thompsonjoshd@gmail.com>
2020-04-10 17:39:26 -0600

    plan out the tests to come with nokogiri_test.rb

--- nokogiri_obstacle_course/nokogiri_test.rb
+++ nokogiri_obstacle_course/nokogiri_test.rb
@@ -3,11 +3,16 @@ require 'nokogiri'
 require 'pry'
 
 class NokogiriTest < MiniTest::Test
+  # every single test listed below has an associated _detailed_ explanation/answer
+  # in the readme at the same level in this repository. 
   
+  # if you get stuck on something, find the right spot in the readme.
+  # or ping me (josh thompson) and I'll help you out!
   
+  ##################################################
+  #     these tests all reference shows.xml        #
+  ##################################################
   def test_shows_xml_lists_all_characters
-    # list of all the characters in all the shows in this document
-    
     # Unless otherwise noted, every file we'll be reading is located
     # within the /docs_to_parse directory. 
     doc = Nokogiri::XML(File.open('docs_to_parse/shows.xml'))
@@ -24,14 +29,15 @@ class NokogiriTest < MiniTest::Test
   end
   
   
-  ### shows.xml
 
   # - list of all the characters in all the shows in this document
   # - Get the characters who performed in Dramas
   # - Get the first drama name back in _four_ different ways
   # - get the names of sitcoms
   # 
-  # ### parts.xml
+  ##################################################
+  #     these tests all reference parts.xml        #
+  ##################################################
   # 
   # - Get all the tires belonging to 'http://aliceautoparts.com/' using `xpath`
   # - Get all the tires belonging to an `xmlns` value of `http://bobsbikes.com/` using `xpath`
@@ -39,23 +45,31 @@ class NokogiriTest < MiniTest::Test
   # - get _just the names of the tires_ in an array
   # 
   # ### atom.xml
+  ##################################################
+  #     these tests all reference atom.xml        #
+  ##################################################
   # 
   # - get all titles using `xpath`
   # - get all titles using `css`
   # 
-  # 
+  ##################################################
+  #   these tests all reference employees.xml      #
+  ##################################################
   # ### employees.xml
   # 
   # - get the full name of the last employee
   # - what is the first employee status?
   # - What is the name of the employee with an inactive status?
   # 
+  #####################################################
+  # these tests all reference josh_works_archive.html #
+  #####################################################
   # ### josh_works_archive.html
   # 
   # - how many links are on the page?
   # - using the `a` css selector, what is the `path` of the last link in the document?
   # - Generate a list of all `href` attributes on the page
   # - Generate an array of strings, representing every single `href` path on the page
-  #  - Generate array of relative_paths, representing all `href`s in the ARCHIVE portion of the page. Don't include non-archive URLs. (should be 221 results long)
+  # - Generate array of relative_paths, representing all `href`s in the ARCHIVE portion of the page. Don't include non-archive URLs. (should be 221 results long)
 
 end
